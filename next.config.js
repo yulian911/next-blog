55aa0c34bb971cf7ae68d796e9db2745545e7a9a
Yulian Melnychuk <julian95@o2.pl>
2023-07-02 16:15:37 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.pexels.com'],
+    domains: ['images.pexels.com', 'via.placeholder.com'],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
